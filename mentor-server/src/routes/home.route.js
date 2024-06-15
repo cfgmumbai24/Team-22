bddba9f12d52b765f6a028889c74b09b910dede4
Mohammad <mohammad.osama4104@gmail.com>
2024-06-16 04:39:33 +0530

    modified:   mentor-server/src/routes/home.route.js

--- mentor-server/src/routes/home.route.js
+++ mentor-server/src/routes/home.route.js
@@ -1,10 +1,8 @@
 import express from "express";
-import getMentorDetails from "../controllers/getMentorDetails.js";
 import addFeedback from "../controllers/addFeedback.js";
 
 const router = express.Router();
 
 router.post('/dashboard/feedback', addFeedback);
-
 
 export default router;
